9f472f29c101764d78a32e7d669e4e4c1fe75caf
natalialexandra <natalia.alexandra195@gmail.com>
2020-02-12 13:02:58 +0100

    import functions from modules and call them

---
+++ modules/index.js
@@ -0,0 +1,9 @@
+// import files
+import { scrollWindow } from "/modules/scroll.js";
+import { clickImages } from "/modules/click.js";
+import { hoverHeader } from "/modules/hover.js";
+
+// call the functions from the modules
+scrollWindow();
+clickImages();
+hoverHeader();
